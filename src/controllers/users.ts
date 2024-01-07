e1a0d908129b64b0a75d0adb2f72adb71ac24ace
Abu Taher <writerabutaher@gmail.com>
2024-01-07 20:54:04 +0600

    delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
-import { getUsers } from "../db/user";
 import express from "express";
+import { deleteUserById, getUsers } from "../db/user";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -9,6 +9,22 @@ export const getAllUsers = async (
     const users = await getUsers();
 
     res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const user = await deleteUserById(id);
+
+    res.status(200).json(user);
   } catch (error) {
     console.error(error);
     return res.sendStatus(400);
